bb04a63e5e85d578a01fa9352a140265d1ce9ed2
Juan David Grijalba Osorio <55849409+juandgo@users.noreply.github.com>
2023-12-30 11:49:48 -0500

    Bugs are fixed

--- TIC-TAC-TOE/game.py
+++ TIC-TAC-TOE/game.py
@@ -1,3 +1,4 @@
+import time
 from player import HumanPlayer, RandomComputerPlayer
 
 class TicTacToe:
@@ -102,7 +103,8 @@ def play(game, x_player, o_player, print_game=True):
             #     letter = 'O'
             # else:
             #     letter = 'X'
-                
+        # tiny break to make things a little bit easier to read
+        time.sleep(0.8)        
     if print_game:
         print('It\'s a tie')        
             
