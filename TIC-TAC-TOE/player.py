ae9b77aa6026df41edf2ae9675e663e9592462bf
Juan David Grijalba Osorio <55849409+juandgo@users.noreply.github.com>
2023-12-30 00:41:46 -0500

    avance titac

--- TIC-TAC-TOE/player.py
+++ TIC-TAC-TOE/player.py
@@ -27,4 +27,11 @@ class HumanPlayer(Player):
         valid_square = False
         val = None 
         while not valid_square:
-            square = input(self.letter)
+            square = input(self.letter+'\'s turn. Input move (0-9)')
+            # we're going to check that this is a correct value by trying to cast
+            # it to an integer, and if it's not, then we say its invalid
+            # if that spot is not available on the beard, we also say its invalid
+            try:
+                val = int(square)
+                if val not in game.available_moves():
+                    raise ValueError
