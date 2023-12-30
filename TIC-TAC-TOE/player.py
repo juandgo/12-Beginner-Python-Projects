a45731d379ca4bd8f3069c6c421369efae16d22d
Juan David Grijalba Osorio <55849409+juandgo@users.noreply.github.com>
2023-12-29 20:19:44 -0500

    creacion del proyecto tic tac toe - tres en linea o triki en colombia

---
+++ TIC-TAC-TOE/player.py
@@ -0,0 +1,25 @@
+import math 
+import random
+
+class Player:
+    def __init__(self, letter):
+        # letter is x or o
+        self.letter = letter
+        
+    # we want all players to get their next move 
+    def get_move(self, game):
+        pass
+    
+class RandomComputerPlayer(Player):
+    def __init__(self, letter):
+        supper().__init__(self, letter)
+    
+    def get_move(self, game):
+        pass
+
+class HumanPlayer(Player):
+    def __init__(self, letter):
+        supper().__init__(self, letter)
+        
+    def get_move(self, game):
+        pass
